3470780741d5e640a16bf81e1aabe731f10c4cc0
akshat luthra <akshatluthra981@gmail.com>
2020-10-13 20:26:00 +0530

    New Project

---
+++ build/precache-manifest.c2411b3a42da7f5cbfefb80b2f139367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f5f166c04befbb1e3f0716d77e4b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fb6533ceaeadc93d546",
+    "url": "/static/css/main.4bc33bc5.chunk.css"
+  },
+  {
+    "revision": "426b68bcf042dd587c9f",
+    "url": "/static/js/2.0327b1c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0327b1c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb6533ceaeadc93d546",
+    "url": "/static/js/main.4c18b9b4.chunk.js"
+  },
+  {
+    "revision": "51d2a0dde69bf6c18b92",
+    "url": "/static/js/runtime-main.69fb69c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
